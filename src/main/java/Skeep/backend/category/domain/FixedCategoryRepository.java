ac7e965101be7d79dbc7c27de102a65dc9480cbe
JeongHeumChoi <andyc707@gmail.com>
2024-07-24 05:00:37 +0900

    Feat: Repository annotation 추가

--- src/main/java/Skeep/backend/category/domain/FixedCategoryRepository.java
+++ src/main/java/Skeep/backend/category/domain/FixedCategoryRepository.java
@@ -1,6 +1,8 @@
 package Skeep.backend.category.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FixedCategoryRepository extends JpaRepository<FixedCategory, Long> {
 }
--- src/main/java/Skeep/backend/location/domain/UserLocationRepository.java
+++ src/main/java/Skeep/backend/location/domain/UserLocationRepository.java
@@ -1,6 +1,8 @@
 package Skeep.backend.location.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserLocationRepository extends JpaRepository<UserLocation, Long> {
 }
