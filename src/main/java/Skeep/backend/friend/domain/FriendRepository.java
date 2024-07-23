db4d91e4e887f2df58eada69c33d745261f2a06e
JeongHeumChoi <79458446+JeongHeumChoi@users.noreply.github.com>
2024-07-24 05:03:00 +0900

    (SKP-91) [Server] 엔티티를 설계한다 (#24)
    * Fix: User 생성 시, 더 엄격하게 예외를 처리할 수 있다.
    
    * Feat: 기본적인 Domain들 구현
    
    * Feat: 유효하지 않은 Fixed Category의 enum이 들어왔을 때의 예외를 처리할 수 있다.
    
    * Feat: Repository annotation 추가

--- src/main/java/Skeep/backend/friend/domain/FriendRepository.java
+++ src/main/java/Skeep/backend/friend/domain/FriendRepository.java
@@ -1,6 +1,8 @@
 package Skeep.backend.friend.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 }
