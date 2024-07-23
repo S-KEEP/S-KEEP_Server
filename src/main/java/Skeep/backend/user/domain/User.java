923f0766c5f0d2390ceaf2148c91a3088293c94a
JeongHeumChoi <andyc707@gmail.com>
2024-07-24 04:45:47 +0900

    Fix: User 생성 시, 더 엄격하게 예외를 처리할 수 있다.

--- src/main/java/Skeep/backend/user/domain/User.java
+++ src/main/java/Skeep/backend/user/domain/User.java
@@ -1,6 +1,5 @@
 package Skeep.backend.user.domain;
 
-
 import Skeep.backend.global.base.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -21,26 +20,29 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "serial_id")
+    @Column(name = "serial_id", nullable = false, unique = true)
     private String serialId;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "provider")
+    @Column(name = "provider", nullable = false)
     @Enumerated(EnumType.STRING)
     private EProvider provider;
 
-    @Column(name = "status")
+    @Column(name = "status", nullable = false)
     @Enumerated(EnumType.STRING)
     private EStatus status;
 
     @Builder
-    public User(String serialId, String name, EProvider provider, EStatus status) {
+    public User(
+            final String serialId,
+            final String name,
+            final EProvider provider
+    ) {
         this.serialId = serialId;
         this.name = name;
         this.provider = provider;
-        this.status = status;
+        this.status = EStatus.ACTIVATED;
     }
-
 }
