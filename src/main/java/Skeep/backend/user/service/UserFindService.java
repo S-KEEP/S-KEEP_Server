d3897b980f419fecd5ec1376800a2a6255ca90c2
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-07-24 23:13:27 +0900

    feat: UserService와 UserFindService 구현

---
+++ src/main/java/Skeep/backend/user/service/UserFindService.java
@@ -0,0 +1,25 @@
+package Skeep.backend.user.service;
+
+import Skeep.backend.global.exception.BaseException;
+import Skeep.backend.user.domain.User;
+import Skeep.backend.user.domain.UserRepository;
+import Skeep.backend.user.exception.UserErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserFindService {
+    private final UserRepository userRepository;
+
+    public Boolean existUserByAppleSerialId(String appleSerialId) {
+        return userRepository.findByAppleSerialId(appleSerialId).isPresent();
+    }
+
+    public User findUserByAppleSerialId(String appleSerialId) {
+        return userRepository.findByAppleSerialId(appleSerialId)
+                .orElseThrow(() -> BaseException.type(UserErrorCode.NOT_FOUND_USER));
+    }
+}
