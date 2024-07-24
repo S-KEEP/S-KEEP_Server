c9e80b8558651d2e3786db9a3256e2ac3376e349
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-07-24 23:24:28 +0900

    refactor: 애플 Public Key Get url 명시

--- src/main/java/Skeep/backend/auth/apple/service/AppleAuthClient.java
+++ src/main/java/Skeep/backend/auth/apple/service/AppleAuthClient.java
@@ -4,8 +4,8 @@ import Skeep.backend.auth.apple.dto.ApplePublicKeys;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "appleAuthClient", url = "${apple.auth.public-key-url}")
+@FeignClient(name = "appleAuthClient", url = "https://appleid.apple.com/auth")
 public interface AppleAuthClient {
-    @GetMapping
+    @GetMapping(value = "/keys")
     ApplePublicKeys getAppleAuthPublicKey();
 }
