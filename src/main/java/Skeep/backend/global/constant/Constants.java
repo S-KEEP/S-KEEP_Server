58c2146c319e05a60618f3865f7fdc67327aa50a
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-07-28 02:15:32 +0900

    setting: swagger를 위해 web config 설정 수정

--- src/main/java/Skeep/backend/global/constant/Constants.java
+++ src/main/java/Skeep/backend/global/constant/Constants.java
@@ -8,8 +8,25 @@ public class Constants {
     public static String PREFIX_AUTH = "authorization";
 
     public static List<String> NO_NEED_AUTH = List.of(
-            "/api/oauth2/sign-up",
-            "/api/oauth2/login/kakao",
-            "/api/oauth2/login/apple"
+            "/api/auth/apple/login",
+            "/swagger-ui/index.html",
+            "/swagger-ui/swagger-ui-standalone-preset.js",
+            "/swagger-ui/swagger-initializer.js",
+            "/swagger-ui/swagger-ui-bundle.js",
+            "/swagger-ui/swagger-ui.css",
+            "/swagger-ui/index.css",
+            "/swagger-ui/favicon-32x32.png",
+            "/swagger-ui/favicon-16x16.png",
+            "/api-docs/json/swagger-config",
+            "/api-docs/json",
+            "/h2-console/**",
+            "/favicon.ico",
+            "/error",
+            "/swagger-ui/**",
+            "/swagger-resources/**",
+            "/v1/api-docs/**",
+            "/v2/api-docs/**",
+            "/v3/api-docs/**",
+            "/v3/api-docs/swagger-config"
     );
 }
