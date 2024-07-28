1aeca746a5723f2bf2f3d702198980f9b2efe694
JeongHeumChoi <andyc707@gmail.com>
2024-07-29 02:48:15 +0900

    Feat: 테스트 코드 틀 작성

---
+++ src/test/java/Skeep/backend/location/domain/LocationRepositoryTest.java
@@ -0,0 +1,7 @@
+package Skeep.backend.location.domain;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("[Repository Test] -> LocationRepository 테스트")
+public class LocationRepositoryTest {
+}
