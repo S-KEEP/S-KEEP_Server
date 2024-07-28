2e19ac43f28f6e207deec7cbdd8e05d8c8f003ed
JeongHeumChoi <andyc707@gmail.com>
2024-07-29 02:12:52 +0900

    Fix: FixedCategory를 Location에 합쳐서 domain을 간소화 한다.

--- src/main/java/Skeep/backend/category/domain/FixedCategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package Skeep.backend.category.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FixedCategoryRepository extends JpaRepository<FixedCategory, Long> {
-}
