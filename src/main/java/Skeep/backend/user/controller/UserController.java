96c76e4b4df7a35be9de180e6cc8e4136910869a
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-07-30 03:40:31 +0900

    feat: 유저 정보 조회 API 구현

--- src/main/java/Skeep/backend/user/controller/UserController.java
+++ src/main/java/Skeep/backend/user/controller/UserController.java
@@ -1,9 +1,13 @@
 package Skeep.backend.user.controller;
 
 import Skeep.backend.global.annotation.UserId;
+import Skeep.backend.user.domain.User;
+import Skeep.backend.user.dto.UserInformation;
+import Skeep.backend.user.service.UserFindService;
 import Skeep.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
+    private final UserFindService userFindService;
 
     @PostMapping
     public ResponseEntity<Void> withdrawalUser(@UserId Long userId) {
         userService.withdrawalUser(userId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<UserInformation> getUserInformation(@UserId Long userId) {
+        User user = userFindService.findById(userId);
+        return ResponseEntity.ok(UserInformation.toUserInformation(user));
     }
 }
