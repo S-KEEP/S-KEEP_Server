a2b5aa7ba3988ee4a4abdd5ea62de3fdc80b5a30
JeongHeumChoi <andyc707@gmail.com>
2024-08-01 00:35:12 +0900

    Feat: 스크린샷을 기반으로 카테고리 및 위치 정보 저장 API를 구현한다

---
+++ src/main/java/Skeep/backend/screenshot/dto/request/ScreenshotUploadDto.java
@@ -0,0 +1,15 @@
+package Skeep.backend.screenshot.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+public class ScreenshotUploadDto implements Serializable {
+        List<MultipartFile> file;
+
+}
