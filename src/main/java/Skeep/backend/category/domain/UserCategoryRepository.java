ee4475ec2059125f3442340570e2a7a1036d74a9
JeongHeumChoi <79458446+JeongHeumChoi@users.noreply.github.com>
2024-08-08 21:25:04 +0900

    (SKP-128) [Server] UserLocation API 버그를 해결한다 (#73)
    * Chore: Redis 설정 credentials 추가
    
    * Fix: 이슈 해결

--- src/main/java/Skeep/backend/category/domain/UserCategoryRepository.java
+++ src/main/java/Skeep/backend/category/domain/UserCategoryRepository.java
@@ -3,6 +3,7 @@ package Skeep.backend.category.domain;
 import Skeep.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserCategoryRepository extends JpaRepository<UserCategory, Long> {
