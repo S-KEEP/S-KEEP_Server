71c7ac8d2848d96d253a6901c069b3113b492c0b
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-08-12 21:00:40 +0900

    feat: ECategory 속성 변경

--- src/main/java/Skeep/backend/category/domain/ECategory.java
+++ src/main/java/Skeep/backend/category/domain/ECategory.java
@@ -10,12 +10,13 @@ import java.util.Arrays;
 @Getter
 @RequiredArgsConstructor
 public enum ECategory {
-    EXCITING("익사이팅", "Activities that are thrilling and exciting"),
-    PARK_NATURE("공원/자연", "Parks and nature-related places"),
-    REST("휴식", "Places ideal for rest and relaxation"),
-    HISTORY("역사 및 유적지", "Historical sites and landmarks"),
-    CULTURE_FESTIVAL("문화/축제", "Cultural events and festivals"),
-    SHOPPING_DOWNTOWN("쇼핑/도심", "Shopping districts and downtown areas")
+    REST("휴식", "방해받지 않고 싶을 때 가려고 모아둔 곳"),
+    PARK_NATURE("공원/자연", "힐링하고 싶을 때"),
+    CULTURE_FESTIVAL("문화/축제", "즐기고 싶은 곳"),
+    SHOPPING_DOWNTOWN("쇼핑/도심", "가까운 곳을 가고 싶을 때"),
+    EXCITING("익사이팅", "뛰어 놀고 싶을 때"),
+    RESTAURANT("맛집", "여기는 꼭 먹어봐야 해"),
+    HISTORY("역사 및 유적지", "언젠간 가볼 곳"),
     ;
 
     private final String name;
