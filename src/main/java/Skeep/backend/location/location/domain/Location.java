a3f4b0b9b908c9ed11f1764fac8c3ced10a7f439
JeongHeumChoi <andyc707@gmail.com>
2024-08-20 00:53:05 +0900

    Feat: 장소 이름 추가

--- src/main/java/Skeep/backend/location/location/domain/Location.java
+++ src/main/java/Skeep/backend/location/location/domain/Location.java
@@ -23,6 +23,9 @@ public class Location extends BaseTimeEntity {
     @Column(name = "kakao_map_id", nullable = false)
     private String kakaoMapId;
 
+    @Column(name = "place_name", nullable = false)
+    private String placeName;
+
     @Column(name = "road_address", nullable = false)
     private String roadAddress;
 
@@ -39,12 +42,14 @@ public class Location extends BaseTimeEntity {
     @Builder
     private Location(
             final String kakaoMapId,
+            final String placeName,
             final String roadAddress,
             final ECategory fixedCategory,
             final String x,
             final String y
     ) {
         this.kakaoMapId = kakaoMapId;
+        this.placeName = placeName;
         this.roadAddress = roadAddress;
         this.x = x;
         this.y = y;
@@ -53,11 +58,13 @@ public class Location extends BaseTimeEntity {
 
     public static Location createLocation(
             String kakaoMapId,
+            String placeName,
             String roadAddress,
             ECategory fixedCategory
     ) {
         return Location.builder()
                 .kakaoMapId(kakaoMapId)
+                .placeName(placeName)
                 .roadAddress(roadAddress)
                 .fixedCategory(fixedCategory)
                 .build();
