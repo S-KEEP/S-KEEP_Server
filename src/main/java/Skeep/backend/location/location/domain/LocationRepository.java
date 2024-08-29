26fd1194758f16e7de838ca6b4721b1436dd4153
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-08-30 03:10:24 +0900

    feat: X, Y 좌표로 Location 검색

--- src/main/java/Skeep/backend/location/location/domain/LocationRepository.java
+++ src/main/java/Skeep/backend/location/location/domain/LocationRepository.java
@@ -2,7 +2,10 @@ package Skeep.backend.location.location.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
     boolean existsByKakaoMapId(String kakaoMapId);
     Location findByKakaoMapId(String kakaoMapId);
+    Optional<Location> findByXAndY(String x, String y);
 }
