4d3713828ec81231cde63175f4ef7af41a41b600
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-09-01 02:45:58 +0900

    refactor: @RequestBody -> @RequestParam

--- src/main/java/Skeep/backend/weather/controller/WeatherController.java
+++ src/main/java/Skeep/backend/weather/controller/WeatherController.java
@@ -7,10 +7,7 @@ import Skeep.backend.weather.service.WeatherLocationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,9 +20,10 @@ public class WeatherController {
     @GetMapping
     public ResponseEntity<WeatherListDto> getWeatherList(
             @UserId Long userId,
-            @RequestBody @Valid WeatherRequest weatherRequest
+            @RequestParam(value = "x") String x,
+            @RequestParam(value = "y") String y
     ) {
-        List<WeatherListDto.WeatherDto> weatherList = weatherLocationService.getWeatherList(weatherRequest.x(), weatherRequest.y());
+        List<WeatherListDto.WeatherDto> weatherList = weatherLocationService.getWeatherList(x, y);
         return ResponseEntity.ok(new WeatherListDto(weatherList));
     }
 }
