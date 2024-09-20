f0d9e34d27a49e39366aff14efd37d3646849c59
JeongHeumChoi <andyc707@gmail.com>
2024-09-20 16:40:44 +0900

    Feat: id값 추가

--- src/main/java/Skeep/backend/fcm/dto/response/FcmNotificationResponseDto.java
+++ src/main/java/Skeep/backend/fcm/dto/response/FcmNotificationResponseDto.java
@@ -5,16 +5,20 @@ import lombok.Builder;
 
 @Builder
 public record FcmNotificationResponseDto(
+        @JsonProperty(value = "id")
+        Long id,
         @JsonProperty(value = "type")
         String type,
         @JsonProperty(value = "url")
         String url
 ) {
         public static FcmNotificationResponseDto of(
+                final Long id,
                 final String type,
                 final String url
         ) {
                 return FcmNotificationResponseDto.builder()
+                                                 .id(id)
                                                  .type(type)
                                                  .url(url)
                                                  .build();
