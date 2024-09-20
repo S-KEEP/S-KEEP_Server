6436bc481e705c2fd9bb9d1c7c186cb21b13b9dc
JeongHeumChoi <andyc707@gmail.com>
2024-09-20 16:42:23 +0900

    Fix: transactional 추가

--- src/main/java/Skeep/backend/friend/service/FriendSaver.java
+++ src/main/java/Skeep/backend/friend/service/FriendSaver.java
@@ -4,12 +4,14 @@ import Skeep.backend.friend.domain.Friend;
 import Skeep.backend.friend.domain.FriendRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class FriendSaver {
     private final FriendRepository friendRepository;
 
+    @Transactional
     public Friend save(Friend friend) {
         return friendRepository.save(friend);
     }
