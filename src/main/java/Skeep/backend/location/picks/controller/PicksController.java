17f7399543b58e8e1491fe1c30a64abc19f8f8bb
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-09-27 23:04:40 +0900

    feat: 에디터 추천 내 카테고리 추가 API 구현

--- src/main/java/Skeep/backend/location/picks/controller/PicksController.java
+++ src/main/java/Skeep/backend/location/picks/controller/PicksController.java
@@ -1,5 +1,6 @@
 package Skeep.backend.location.picks.controller;
 
+import Skeep.backend.global.annotation.UserId;
 import Skeep.backend.location.picks.dto.request.PicksRequest;
 import Skeep.backend.location.picks.dto.response.PicksDtoList;
 import Skeep.backend.location.picks.service.PicksService;
@@ -7,6 +8,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +32,14 @@ public class PicksController {
     public ResponseEntity<Void> deleteAll() {
         picksService.deleteAll();
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/user-category")
+    public ResponseEntity<Void> addUserCategory(
+            @UserId Long userId,
+            @RequestParam("userCategoryId") Long userCategoryId,
+            @RequestParam("title") String title) {
+        Long userLocationId = picksService.addUserCategory(userId, userCategoryId, title);
+        return ResponseEntity.created(URI.create("/api/user-location/" + userLocationId)).build();
     }
 }
