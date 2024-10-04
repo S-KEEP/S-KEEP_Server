a01735e882cecac4d0103519a3991200cc9502b7
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-10-05 02:43:53 +0900

    refactor: 삭제 메서드 @Modifying 추가

--- src/main/java/Skeep/backend/notification/domain/CategoryNotification/CategoryNotificationRepository.java
+++ src/main/java/Skeep/backend/notification/domain/CategoryNotification/CategoryNotificationRepository.java
@@ -2,12 +2,22 @@ package Skeep.backend.notification.domain.CategoryNotification;
 
 import Skeep.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CategoryNotificationRepository extends JpaRepository<CategoryNotification, Long> {
+    // query method
     List<CategoryNotification> findAllByUser(User user);
     Optional<CategoryNotification> findByUserAndId(User user, Long id);
-    void deleteByUser(User user);
+
+    // @Query
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM CategoryNotification cn WHERE cn.user = :user")
+    void deleteByUser(@Param("user") User user);
 }
--- src/main/java/Skeep/backend/notification/domain/UserLocationNotification/UserLocationNotificationRepository.java
+++ src/main/java/Skeep/backend/notification/domain/UserLocationNotification/UserLocationNotificationRepository.java
@@ -2,12 +2,22 @@ package Skeep.backend.notification.domain.UserLocationNotification;
 
 import Skeep.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserLocationNotificationRepository extends JpaRepository<UserLocationNotification, Long> {
+    // query method
     List<UserLocationNotification> findAllByUser(User user);
     Optional<UserLocationNotification> findByUserAndId(User user, Long id);
-    void deleteByUser(User user);
+
+    // @Query
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM UserLocationNotification uln WHERE uln.user = :user")
+    void deleteByUser(@Param("user") User user);
 }
